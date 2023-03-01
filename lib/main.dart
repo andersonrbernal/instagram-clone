a76c4ed530381895fd0ec909bd97b7bbd35c9c66
Anderson <andersonbernal2017@gmail.com>
2023-03-01 15:54:21 -0400

    feat(widget): Add bottom app bar

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Insta App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -27,13 +28,75 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int currentPage = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Insta App'),
       ),
-      body: Container(),
+      body: Scaffold(
+        bottomNavigationBar: BottomAppBar(
+            child: Row(children: [
+          const Spacer(),
+          IconButton(
+              icon: Icon(Icons.home,
+                  color: currentPage == 0
+                      ? const Color.fromRGBO(203, 73, 101, 1)
+                      : const Color.fromRGBO(40, 40, 40, 1)),
+              onPressed: () {
+                setState(() {
+                  currentPage = 0;
+                });
+              }),
+          const Spacer(),
+          IconButton(
+              icon: Icon(Icons.search,
+                  color: currentPage == 1
+                      ? const Color.fromRGBO(203, 73, 101, 1)
+                      : const Color.fromRGBO(40, 40, 40, 1)),
+              onPressed: () {
+                setState(() {
+                  currentPage = 1;
+                });
+              }),
+          const Spacer(),
+          IconButton(
+              icon: Icon(Icons.ondemand_video,
+                  color: currentPage == 2
+                      ? const Color.fromRGBO(203, 73, 101, 1)
+                      : const Color.fromRGBO(40, 40, 40, 1)),
+              onPressed: () {
+                setState(() {
+                  currentPage = 2;
+                });
+              }),
+          const Spacer(),
+          IconButton(
+              icon: Icon(Icons.card_travel,
+                  color: currentPage == 3
+                      ? const Color.fromRGBO(203, 73, 101, 1)
+                      : const Color.fromRGBO(40, 40, 40, 1)),
+              onPressed: () {
+                setState(() {
+                  currentPage = 3;
+                });
+              }),
+          const Spacer(),
+          IconButton(
+              icon: Icon(Icons.person,
+                  color: currentPage == 4
+                      ? const Color.fromRGBO(203, 73, 101, 1)
+                      : const Color.fromRGBO(40, 40, 40, 1)),
+              onPressed: () {
+                setState(() {
+                  currentPage = 4;
+                });
+              }),
+          const Spacer()
+        ])),
+      ),
     );
   }
 }
